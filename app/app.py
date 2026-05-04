f59c34c65b293ee079f9112aac53329dacbd3645
Edson Augusto <edsonaugustotl@gmail.com>
2026-05-04 14:44:26 -0400

    adicionando classe de cores

---
+++ app/app.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.patches import Rectangle, Circle, Wedge, Polygon
+
+import numpy as np
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+from enum import Enum
+
+class Color(Enum):
+  gray = "#5e5c64"          # Cor Cinza
+  light_gray = "#868687"    # Cor Cinza Claro
+  dark_blue = "#18304a"     # Cor Azul Escuro
+  light_blue = "#0251a1"    # Cor Azul Claro
+  aqua_blue = "#033f70"     # Cor Verde Agua
+  white = "#e8e8ed"         # Cor White
+  black = "#000000"         # Cor Preto
+
+class App:
+    def __init__():
+        return
