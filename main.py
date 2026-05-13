84632129f06c5b90cec8dc67a2e49823401e3ef1
Victor-Filgueiras <vfilgueiras.dev@gmail.com>
2026-05-13 15:19:57 -0400

    Simplify main function by removing theme setup
    Removed theme styling setup from main function.

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import tkinter as tk
-from tkinter import ttk
 from gui import Aplicacao
 
 def main():
     root = tk.Tk()
-    
-    style = ttk.Style()
-    if 'clam' in style.theme_names():
-        style.theme_use('clam')
-        
     app = Aplicacao(root)
     root.mainloop()
 
